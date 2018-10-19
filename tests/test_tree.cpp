3fa692b1ef9b92de1cf0db56477a5ec394792736
Mark Melton <mark.melton@notlem.com>
2018-10-18 23:55:57 -0500

    Checkpoint

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -12,31 +12,44 @@ using ::testing::StaticAssertTypeEq;
 
 using namespace peg;
 
-struct TreeState
-{ };
+struct Ignored : Optional<WhiteSpace> { };
+struct Number : Sequence<Ignored,Optional<Choice<Plus,Minus>>,OneOrMore<DecimalDigit>> { };
+struct Infix : Choice<Minus,Plus,Times,Divide> { };
+struct Atomic : Number { };
+struct Expression : Sequence<List<Atomic,Infix,Ignored>,Eof> { };
+
+using TreeState = std::vector<std::string>;
+
+template<typename T>
+constexpr bool is_tree_node = std::is_same<T,Atomic>::value
+    || std::is_same<T,Infix>::value
+    || std::is_same<T,Expression>::value
+    ;
+
+template<typename T>
+struct Selector
+{ constexpr static bool value = is_tree_node<T>; };
 
 template<typename Parser>
 struct TreeControl
 {
     static void start(const Input& input, TreeState& state)
     {
+	if constexpr (not Selector<Parser>::value) return;
+	state.push_back(std::string(get_name<Parser>()));
     }
 
     static void success(const Input& input, TreeState& state)
     {
+	if constexpr (not Selector<Parser>::value) return;
     }
 
     static void failure(const Input& input, TreeState& state)
     {
+	if constexpr (not Selector<Parser>::value) return;
+	state.pop_back();
     }
 };
-
-using Ignored = Optional<WhiteSpace>;
-using Number = Sequence<Ignored,Optional<Choice<Plus,Minus>>,OneOrMore<DecimalDigit>>;
-using Infix = Choice<Minus,Plus,Times,Divide>;
-using Atomic = Number;
-using Expression = Sequence<List<Atomic,Infix,Ignored>,Eof>;
-
 
 TEST(Peg, TreeControl0)
 {
@@ -56,6 +69,9 @@ TEST(Peg, TreeControl1)
     auto r = Machine<Parser,TreeControl>::match(s, state);
     EXPECT_TRUE(r);
     EXPECT_EQ(r.match(), "1 + 1");
+
+    for (const auto& s : state)
+	cout << s << endl;
 }
 
 int main(int argc, char *argv[])
