d388ac5b7cb2718a099f1fb0899f1c62a35b3c15
Mark Melton <mark.melton@notlem.com>
2019-02-17 21:52:16 -0600

    WIP: Combinators for C parser.

---
+++ src/tools/t1.cpp
@@ -0,0 +1,98 @@
+// Copyright (C) 2019 by Mark Melton
+//
+
+#include <fmt/format.h>
+#include <stack>
+#include "core/tool.h"
+#include "core/demangle.h"
+#include "peg/peg.h"
+
+using namespace peg;
+
+struct Environment
+{
+    Environment()
+	: env(10)
+    { }
+
+    template<size_t ID>
+    void push(string_view s) { env[ID].push(s); }
+
+    template<size_t ID>
+    auto top() { return env[ID].top(); }
+
+    template<size_t ID>
+    string_view pop()
+    {
+	auto r = env[ID].top();
+	env[ID].pop();
+	return r;
+    }
+	
+    vector<std::stack<string_view>> env;
+};
+
+template<class P, size_t ID>
+struct MatchPush
+{
+    template<template<class> class... Actions, class... States>
+    static Input match(const Input& input, Environment& env, States&... states)
+    {
+	auto r = Control::template match<P, Actions...>(input, env, states...);
+	if (not r) return r;
+
+	env.template push<ID>(r.match_view());
+	return r;
+    }
+};
+
+template<class P, size_t ID>
+struct MatchPop
+{
+    template<template<class> class... Actions, class... States>
+    static Input match(const Input& input, Environment& env, States&... states)
+    {
+	auto r = Control::template match<P, Actions...>(input, env, states...);
+	if (not r) return r;
+
+	auto str = env.template top<ID>();
+	if (str != r.match_view())
+	    return input.failure();
+	env.template pop<ID>();
+	return r;
+    }
+};
+
+struct Digit : Range<'0','9'> {};
+struct NonZeroDigit : Range<'1','9'> {};
+struct DecimalConstant : Sequence<NonZeroDigit, ZeroOrMore<Digit>> {};
+
+struct OctalDigit : Range<'0','7'> {};
+struct OctalConstant : Sequence<Character<'0'>, ZeroOrMore<OctalDigit>> {};
+
+struct HexadecimalDigit : Range<'0','9','a','f','A','F'> {};
+struct HexadecimalPrefix : Sequence<Character<'0'>, Choice<Character<'x'>, Character<'X'>>> {};
+struct HexadecimalConstant : Sequence<HexadecimalPrefix, OneOrMore<HexadecimalDigit>> {};
+
+struct HexQuad: Sequence<HexadecimalDigit, HexadecimalDigit, HexadecimalDigit, HexadecimalDigit>{};
+struct UniversalCharacterName : Sequence<c::Backslash, Range<'u','u','U','U'>, HexQuad> {};
+
+struct IdentifierDigit : Choice<Range<'a','z','A','Z','_','_','0','9'>, UniversalCharacterName> {};
+struct IdentifierNonDigit : Choice<Range<'a','z','A','Z','_','_'>, UniversalCharacterName> {};
+struct Identifier : Sequence<IdentifierNonDigit, ZeroOrMore<IdentifierDigit>> {};
+
+int tool_main(int argc, const char *argv[])
+{
+    core::POpt opts;
+    opts.process(argc, argv);
+    cout << std::boolalpha;
+
+    for (auto input : opts.extra())
+    {
+	auto state = Environment{};
+	auto r = parse<Identifier, DebugAction>(input, state);
+	cout << input << ": " << (bool)r << " " << r.match() << endl;
+    }
+    return 0;
+}
+
